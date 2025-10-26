b181ce0b9e69fda26e9a45f1696adb6c8466dd7b
sankar kalla <gowrisankarkalla4@gmail.com>
2025-10-26 08:54:05 +0530

    added suspend for /chat/courses

--- app/(dashboard)/chat/courses/page.tsx
+++ app/(dashboard)/chat/courses/page.tsx
@@ -1,9 +1,12 @@
 import ChatCoursesPagesView from "@/modules/chat-with-course/ui/views/chat-with-course-page";
+import { Suspense } from "react";
 
 const ChatCourses = () => {
-
-  
-  return <ChatCoursesPagesView />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ChatCoursesPagesView />
+    </Suspense>
+  );
 };
 
 export default ChatCourses;
