f36693ab27c324218d22799699cae4e6292f5182
sankar kalla <gowrisankarkalla4@gmail.com>
2025-10-31 16:57:32 +0530

    remove: prefetching chat/courses

--- app/(dashboard)/chat/courses/page.tsx
+++ app/(dashboard)/chat/courses/page.tsx
@@ -1,19 +1,11 @@
-import { useCoursesParams } from "@/hooks/use-courses-params";
+
 import ChatCoursesPagesView from "@/modules/chat-with-course/ui/views/chat-with-course-page";
-import { getAllUserCourses } from "@/modules/courses/server/courses";
 import { redirectUnauthrizedUser } from "@/server/user";
-import { QueryClient } from "@tanstack/react-query";
 import { Suspense } from "react";
 
 const ChatCourses = async () => {
   await redirectUnauthrizedUser();
 
-  const [params] = useCoursesParams();
-  const queryClient = new QueryClient();
-  void queryClient.prefetchQuery({
-    queryKey: ["get-user-courses-by-params", params],
-    queryFn: async () => await getAllUserCourses(params),
-  });
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <ChatCoursesPagesView />
